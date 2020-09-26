7d07abc973e48f955f87f044708db8b76b5af91a
nominalrune <59037577+nominalrune@users.noreply.github.com>
2020-09-26 13:44:45 +0900

    Create default.sh
    一気通貫用

---
+++ default.sh
@@ -0,0 +1,6 @@
+mkdir /projects
+mkdir /projects/iroha
+git clone https://github.com/nominalrune/iroha-board-Dockerized.git /projects/iroha
+cd /projects/iroha
+git checkout -b lbProjectTest
+docker-compose up --build
