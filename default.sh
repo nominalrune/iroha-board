f58d5e0f2cc7e87f480cd00d4117d40426632fc5
nominalrune <59037577+nominalrune@users.noreply.github.com>
2020-09-27 14:41:51 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -3,6 +3,6 @@ mkdir /projects/iroha
 git clone https://github.com/nominalrune/iroha-board-Dockerized.git /projects/iroha
 cd /projects/iroha
 git checkout -b lbProjectTest
+docker-compose build
 chown -R www-data /projects/iroha/apache
-docker-compose up --build
-
+docker-compose up
